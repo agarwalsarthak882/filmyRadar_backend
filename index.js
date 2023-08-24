4b7b6ba464ed8415b763dac8f4d81469c065d0b9
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-08-24 18:15:05 +0530

    updated backend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getMediaList = require('./getMediaList');
 //Code.
 const app = express();
 app.use(cors({
-  origin: ('http://localhost:5173'||'https://filmyradar.netlify.app/') // Replace with your frontend's origin
+  origin: ('http://localhost:5173'||'https://filmyradar.netlify.app') // Replace with your frontend's origin
 }));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
