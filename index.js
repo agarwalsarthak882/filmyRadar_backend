a5e0d7c2083a35c2f0d7abe7986acbd5ac878d02
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-10-21 12:49:31 +0530

    new commit

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const { default: axios } = require('axios');
 //Code.
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173','https://filmyradar.netlify.app','https://main--filmyradar.netlify.app','https://64f3919cf563cd00085f8d59--filmyradar.netlify.app'] // Replace with your frontend's origin
+  origin: ['http://localhost:5173', 'https://filmyradar.netlify.app', 'https://main--filmyradar.netlify.app', 'https://64f3919cf563cd00085f8d59--filmyradar.netlify.app'] // Replace with your frontend's origin
 }));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-let port=process.env.PORT
-app.listen(8000||port);
+let port = process.env.PORT
+app.listen(8000 || port);
 
 const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri);
@@ -31,10 +31,10 @@ getMediaList(app);
 
 function pingLink() {
   const linkToPing = 'https://filmyradar-backend.onrender.com/movie/top_rated'; // Replace with the link you want to ping
-let data=axios.get(linkToPing)
-data.then(res=>{})
+  let data = axios.get(linkToPing)
+  data.then(res => { })
 }
 
 // Ping the link every 10 minutes (10 minutes = 600,000 milliseconds)
-const pingInterval = 10*60*1000;
+const pingInterval = 10 * 60 * 1000;
 setInterval(pingLink, pingInterval);
